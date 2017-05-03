97ba5a7524e6ccc98e6265e82349df4d0e4b060c
Tomasz Ratajczak <twratajczak@gmail.com>
2017-05-03 13:04:25 +0200

    submit values

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=mipsel-openwrt-linux-gcc
 
-gpio: export STAGING_DIR=$(HOME)/Documents/onion/lede-toolchain-ramips-mt7688_gcc-5.4.0_musl.Linux-x86_64/toolchain-mipsel_24kc_gcc-5.4.0_musl/
+gpio: export STAGING_DIR=$(HOME)/Documents/onion/lede/staging_dir/target-mipsel_24kc_musl/
 gpio: gpio.c
-	$(CC) $^ -o $@
+	$(CC) -lcurl $^ -o $@
 	scp $@ onion:
 
--- gpio.c
+++ gpio.c
@@ -1,14 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <poll.h>
+#include <curl/curl.h>
+#include <string.h>
 
 #define NUM 4
 
-int i;
+int i, len;
 char buf[1023];
 FILE *files[NUM];
 struct pollfd fds[NUM] = {};
 char values[NUM];
+
+CURL *ch;
+CURLcode res;
 
 void setup(int i, int n) {
 	FILE *export = fopen("/sys/class/gpio/export", "w");
@@ -30,6 +35,22 @@ void setup(int i, int n) {
 	fds[i].events = POLLPRI | POLLERR;
 }
 
+void submit() {
+	strcpy(buf, "http://example.com?gpio=");
+	len = strlen(buf);
+	for (i = 0; i < NUM; ++i) {
+		buf[len++] = values[i];
+		buf[len++] = ',';
+	}
+	buf[len] = '\0';
+	ch = curl_easy_init();
+	curl_easy_setopt(ch, CURLOPT_URL, buf);
+	res = curl_easy_perform(ch);
+	if (res != CURLE_OK)
+		fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+	curl_easy_cleanup(ch);
+}
+
 int main(int argc, char **argv) {
 	for (i = 0; i < NUM; ++i)
 		setup(i, 0 + i);
@@ -39,7 +60,8 @@ int main(int argc, char **argv) {
 			fseek(files[i], 0, SEEK_SET);
 			fread(&values[i], sizeof(values[i]), 1, files[i]);
 		}
-		poll(fds, NUM, 1000 * 30);
+		submit();
+		poll(fds, NUM, 1000 * 60 * 15);
 	}
 
 	for (i = 0; i < NUM; ++i)
