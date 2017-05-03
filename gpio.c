6e1a792803330c636852f8b0b2e8b75e03178cab
Tomasz Ratajczak <twratajczak@gmail.com>
2017-05-03 15:46:53 +0200

    submit values with POST

--- gpio.c
+++ gpio.c
@@ -10,14 +10,14 @@ int num = 0;
 int num_map[MAX_NUM];
 
 int i, len;
-char buf[4096];
+char buf[4096], *bufp;
 FILE *files[MAX_NUM];
 struct pollfd fds[MAX_NUM] = {};
 char values[MAX_NUM];
-int errors = 0;
 
 CURL *ch;
 CURLcode res;
+int curl_errors;
 
 char *url_template = "%s&e=%d&gpio=";
 char url[1024] = "";
@@ -26,9 +26,8 @@ int poll_timeout_ms = 0;
 struct uci_ptr uci_ptr;
 struct uci_context *uci_ctx;
 char uci_path[256];
-char *uci_buf;
 
-int setup_config() {
+int process_config() {
 	uci_ctx = uci_alloc_context();
 	if (!uci_ctx) {
 		fprintf(stderr, "Failed to alloc UCI ctx\n");
@@ -56,8 +55,8 @@ int setup_config() {
 	if (uci_ptr.flags & UCI_LOOKUP_COMPLETE) {
 		strncpy(buf, uci_ptr.o->v.string, 1023);
 		buf[1024] = '\0';
-		for (uci_buf = strtok(buf, " "); uci_buf; uci_buf = strtok(NULL, " "))
-			num_map[num++] = atoi(uci_buf);
+		for (bufp = strtok(buf, " "); bufp; bufp = strtok(NULL, " "))
+			num_map[num++] = atoi(bufp);
 	}
 
 	uci_free_context(uci_ctx);
@@ -84,27 +83,49 @@ void setup_gpio(int i) {
 	fds[i].events = POLLPRI | POLLERR;
 }
 
-void submit() {
-	sprintf(buf, url_template, url, errors);
-	len = strlen(buf);
-	for (i = 0; i < num; ++i) {
-		buf[len++] = values[i];
-		buf[len++] = ',';
-	}
-	buf[len] = '\0';
+void cleanup_gpio(int i) {
+	FILE *unexport = fopen("/sys/class/gpio/unexport", "w");
+	fprintf(unexport, "%d", num_map[i]);
+	fclose(unexport);
+	fclose(files[i]);
+}
+
+int setup_curl() {
 	ch = curl_easy_init();
-	curl_easy_setopt(ch, CURLOPT_URL, buf);
-	res = curl_easy_perform(ch);
-	if (res != CURLE_OK)
-		++errors;
-	else
-		errors = 0;
+	if (!ch)
+		return 1;
+	curl_easy_setopt(ch, CURLOPT_URL, url);
+	curl_errors = 0;
+	return 0;
+}
+
+void cleanup_curl() {
 	curl_easy_cleanup(ch);
 }
 
+void submit() {
+	bufp = buf;
+	sprintf(bufp, "e=%d", curl_errors);
+	bufp += strlen(bufp);
+
+	for (i = 0; i < num; ++i) {
+		sprintf(bufp, "&gpio-%d=%c", num_map[i], values[i]);
+		bufp += strlen(bufp);
+	}
+	curl_easy_setopt(ch, CURLOPT_POSTFIELDS, buf);
+	res = curl_easy_perform(ch);
+	if (res != CURLE_OK)
+		++curl_errors;
+	else
+		curl_errors = 0;
+}
+
 int main(int argc, char **argv) {
-	if (setup_config())
+	if (process_config())
 		return 1;
+
+	if (setup_curl())
+		return 2;
 
 	for (i = 0; i < num; ++i)
 		setup_gpio(i);
@@ -118,8 +139,10 @@ int main(int argc, char **argv) {
 		poll(fds, num, poll_timeout_ms);
 	}
 
+	cleanup_curl();
+
 	for (i = 0; i < num; ++i)
-		fclose(files[i]);
+		cleanup_gpio(i);
 
 	return 0;
 }
