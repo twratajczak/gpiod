614de484fea21329764a395c9e7f33dc4762e964
Tomasz Ratajczak <twratajczak@gmail.com>
2017-05-03 15:50:35 +0200

    cleanup

--- gpiod.c
+++ gpiod.c
@@ -6,8 +6,8 @@
 #include <uci.h>
 
 #define MAX_NUM 16
-int num = 0;
-int num_map[MAX_NUM];
+int gpio_num = 0;
+int gpio_map[MAX_NUM];
 
 int i, len;
 char buf[4096], *bufp;
@@ -15,11 +15,10 @@ FILE *files[MAX_NUM];
 struct pollfd fds[MAX_NUM] = {};
 char values[MAX_NUM];
 
-CURL *ch;
-CURLcode res;
+CURL *curl_ctx;
+CURLcode curl_res;
 int curl_errors;
 
-char *url_template = "%s&e=%d&gpio=";
 char url[1024] = "";
 int poll_timeout_ms = 0;
 
@@ -56,7 +55,7 @@ int process_config() {
 		strncpy(buf, uci_ptr.o->v.string, 1023);
 		buf[1024] = '\0';
 		for (bufp = strtok(buf, " "); bufp; bufp = strtok(NULL, " "))
-			num_map[num++] = atoi(bufp);
+			gpio_map[gpio_num++] = atoi(bufp);
 	}
 
 	uci_free_context(uci_ctx);
@@ -65,19 +64,19 @@ int process_config() {
 
 void setup_gpio(int i) {
 	FILE *export = fopen("/sys/class/gpio/export", "w");
-	fprintf(export, "%d", num_map[i]);
+	fprintf(export, "%d", gpio_map[i]);
 	fclose(export);
 
-	sprintf(buf, "/sys/class/gpio/gpio%d/direction", num_map[i]);
+	sprintf(buf, "/sys/class/gpio/gpio%d/direction", gpio_map[i]);
 	FILE *direction = fopen(buf, "w");
 	fprintf(direction, "in");
 	fclose(direction);
-	sprintf(buf, "/sys/class/gpio/gpio%d/edge", num_map[i]);
+	sprintf(buf, "/sys/class/gpio/gpio%d/edge", gpio_map[i]);
 	FILE *edge = fopen(buf, "w");
 	fprintf(edge, "both");
 	fclose(edge);
 
-	sprintf(buf, "/sys/class/gpio/gpio%d/value", num_map[i]);
+	sprintf(buf, "/sys/class/gpio/gpio%d/value", gpio_map[i]);
 	files[i] = fopen(buf, "r");
 	fds[i].fd = fileno(files[i]);
 	fds[i].events = POLLPRI | POLLERR;
@@ -85,22 +84,22 @@ void setup_gpio(int i) {
 
 void cleanup_gpio(int i) {
 	FILE *unexport = fopen("/sys/class/gpio/unexport", "w");
-	fprintf(unexport, "%d", num_map[i]);
+	fprintf(unexport, "%d", gpio_map[i]);
 	fclose(unexport);
 	fclose(files[i]);
 }
 
 int setup_curl() {
-	ch = curl_easy_init();
-	if (!ch)
+	curl_ctx = curl_easy_init();
+	if (!curl_ctx)
 		return 1;
-	curl_easy_setopt(ch, CURLOPT_URL, url);
+	curl_easy_setopt(curl_ctx, CURLOPT_URL, url);
 	curl_errors = 0;
 	return 0;
 }
 
 void cleanup_curl() {
-	curl_easy_cleanup(ch);
+	curl_easy_cleanup(curl_ctx);
 }
 
 void submit() {
@@ -108,13 +107,13 @@ void submit() {
 	sprintf(bufp, "e=%d", curl_errors);
 	bufp += strlen(bufp);
 
-	for (i = 0; i < num; ++i) {
-		sprintf(bufp, "&gpio-%d=%c", num_map[i], values[i]);
+	for (i = 0; i < gpio_num; ++i) {
+		sprintf(bufp, "&gpio%d=%c", gpio_map[i], values[i]);
 		bufp += strlen(bufp);
 	}
-	curl_easy_setopt(ch, CURLOPT_POSTFIELDS, buf);
-	res = curl_easy_perform(ch);
-	if (res != CURLE_OK)
+	curl_easy_setopt(curl_ctx, CURLOPT_POSTFIELDS, buf);
+	curl_res = curl_easy_perform(curl_ctx);
+	if (curl_res != CURLE_OK)
 		++curl_errors;
 	else
 		curl_errors = 0;
@@ -127,21 +126,21 @@ int main(int argc, char **argv) {
 	if (setup_curl())
 		return 2;
 
-	for (i = 0; i < num; ++i)
+	for (i = 0; i < gpio_num; ++i)
 		setup_gpio(i);
 
-	while (num) {
-		for (i = 0; i < num; ++i) {
+	while (gpio_num) {
+		for (i = 0; i < gpio_num; ++i) {
 			fseek(files[i], 0, SEEK_SET);
 			fread(&values[i], sizeof(values[i]), 1, files[i]);
 		}
 		submit();
-		poll(fds, num, poll_timeout_ms);
+		poll(fds, gpio_num, poll_timeout_ms);
 	}
 
 	cleanup_curl();
 
-	for (i = 0; i < num; ++i)
+	for (i = 0; i < gpio_num; ++i)
 		cleanup_gpio(i);
 
 	return 0;
